957e6aec8859ea5b3518006fe7c77674d135a77a
Soviatul Qhoziyah <soviatulqhoziyah13@gmail.com>
2025-07-03 17:21:20 +0700

    database beasiswa

--- resources/views/layouts/tamplate.blade.php
+++ resources/views/layouts/tamplate.blade.php
@@ -253,6 +253,12 @@
                         <li class="nav-item">
                             <a class="nav-link {{ request()->routeIs('home.homepage') ? 'active' : '' }}"
                                 href="{{ route('home.homepage') }}">
+                                <i class="fas fa-calendar-alt me-1"></i>Beasiswa
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->routeIs('home.homepage') ? 'active' : '' }}"
+                                href="{{ route('home.homepage') }}">
                                 <i class="fas fa-calendar-alt me-1"></i>Events
                             </a>
                         </li>
